e33ae789ef94383458af12d41aae010bcd5c680f
AleksandrBocharov <bigtwopizza@gmail.com>
2023-01-10 10:46:49 +0200

    First commit

--- Java_Course/src/HelloWorld.java
+++ Java_Course/src/HelloWorld.java
@@ -1,7 +1,5 @@
-public class Hello_World {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         System.out.println("Hello World");
     }
 }
-
